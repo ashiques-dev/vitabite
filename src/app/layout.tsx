60cc80dd8b591b995eefb2ee503f23fdf32b29ed
ashiques-dev <ashiques.dev@gmail.com>
2025-03-21 04:54:52 +0530

    navbar and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Urbanist } from "next/font/google";
-
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 const urbanist = Urbanist({ variable: "--font-urbanist", subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth" suppressHydrationWarning>
-      <body className={urbanist.variable}>{children}</body>
+      <body className={urbanist.variable}>
+        <Navbar />
+
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
